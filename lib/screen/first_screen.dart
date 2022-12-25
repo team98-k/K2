0ba323d138160eb6191339183dae5b1d0e681147
Kim <wow4090@gmail.com>
2022-12-25 14:01:03 +0900

    1.0.0

---
+++ lib/screen/first_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class FirstScreen extends StatefulWidget {
+  const FirstScreen({Key? key}) : super(key: key);
+
+  @override
+  State<FirstScreen> createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("김필중"),
+      ),
+      body: Container(
+        child: TextButton(onPressed: (){}, child: Text("바보"), ),
+      ),
+    );
+  }
+}
