dc6133b8fa9072e9dcb5ae150ceb530455508195
neogulcity <121531764+8bitSweetroll@users.noreply.github.com>
2022-12-31 17:48:34 +0900

    bottomNavigationBar 추가

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -43,6 +43,25 @@ class HomeScreen extends StatelessWidget {
           );
         },
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+          ),
+        ],
+        // currentIndex: _selectedIndex,
+        // selectedItemColor: Colors.amber[800],
+        // onTap: _onItemTapped,
+      ),
     );
   }
 
